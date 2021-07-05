6b65a636b34df79d3bef8d536b08891979a91f42
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-06 11:39:26 +1200

    setup of Addtodo component

--- client/components/AddTodo.jsx
+++ client/components/AddTodo.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+// import the action
+import { saveTodo } from '../actions'
 
 function AddTodo (props) {
+  const [todo, setTodo] = useState('')
+
+  // handleChange
+  function handleChange (evt) {
+    setTodo(evt.target.value)
+  }
+
+  // handleSubmit
+  function handleSubmit (evt) {
+    evt.preventDefault()
+    props.dispatch(saveTodo(todo))
+  }
   return (
     <>
-      <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} />
+      <form onSubmit={handleSubmit}>
+        <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} value={todo} onChange={handleChange}/>
+      </form>
     </>
   )
 }
 
-export default AddTodo
+export default connect()(AddTodo)
