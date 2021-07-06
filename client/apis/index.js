acbaa43ad82904c5fef0ec987691b4c883728722
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-06 16:14:06 +1200

    api function for displaying added tasks

--- client/apis/index.js
+++ client/apis/index.js
@@ -1,8 +1,19 @@
 import request from 'superagent'
 
+const rootUrl = '/ap1/v1'
+
+export function fetchTodos () {
+  return request.get(rootUrl + 'tasks')
+    .then(res => {
+      return res.body
+    })
+    .catch(e =>
+      console.log(e))
+}
+
 export function createTodo (todo) {
   return request
-    .post('/api/v1/todos')
+    .post(rootUrl + '/todos')
     .send({ todo })
     .then(res => {
       return res.body
