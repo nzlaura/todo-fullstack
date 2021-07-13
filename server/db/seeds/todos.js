ffc54d8680966089c62e8a2cbcc18da98cbe4423
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-13 14:57:39 +1200

    updated seeds for priority and completion

--- server/db/seeds/todos.js
+++ server/db/seeds/todos.js
@@ -4,9 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('todos').insert([
-        { id: 1, task: 'clean car' },
-        { id: 2, task: 'do dishes' },
-        { id: 3, task: 'exercise' }
+        { id: 1, task: 'clean car', priority: 'Low', completed: 'yes' },
+        { id: 2, task: 'do dishes', priority: 'High', completed: 'no' },
+        { id: 3, task: 'exercise', priority: 'Low', completed: 'no' }
       ])
     })
 }
