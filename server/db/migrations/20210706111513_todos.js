345e94be94aa442b1d66a8b5ebed241da7d7950b
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-13 14:56:20 +1200

    updated migrations for priority and completion

--- server/db/migrations/20210706111513_todos.js
+++ server/db/migrations/20210706111513_todos.js
@@ -2,6 +2,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('todos', table => {
     table.increments('id')
     table.string('task')
+    table.string('priority')
+    table.string('completed')
   })
 }
 
